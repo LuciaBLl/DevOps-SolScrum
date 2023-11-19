9da44551743d870f3a4c513b29ee7b5284e19427
Lucia Brenes Lluna <luciabreneslluna@MacBook-Air-de-Lucia-2.local>
2023-11-19 15:17:11 +0100

    Modificaciones suma y test_suma

--- test_suma.py
+++ test_suma.py
@@ -1,12 +1,30 @@
 import unittest
 
-from suma import sumar
+from suma import sumar, restar, multiplicar, dividir
 
 class TestSumar(unittest.TestCase):
+
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+
+    def test_resta(self):
+        self.assertEqual(restar(5, 3), 2)
+        self.assertEqual(restar(0, 5), -5)
+        self.assertEqual(restar(-2, -2), 0)
+
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicar(2, 3), 6)
+        self.assertEqual(multiplicar(0, 5), 0)
+        self.assertEqual(multiplicar(-2, 4), -8)
+
+    def test_division(self):
+        self.assertEqual(dividir(6, 3), 2.0)
+        self.assertEqual(dividir(5, 2), 2.5)
+        self.assertEqual(dividir(0, 5), 0)
+        self.assertEqual(dividir(-8, 2), -4.0)
+
 
 if __name__ == '__main__':
     unittest.main()
